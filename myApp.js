746f10a7dbf16eec5693a4f6a6df63b5d98c2466
Islam Dudaev <islamwolf23@gmail.com>
2016-11-06 17:04:23 +0300

    Second atempt
    Side bar without projects. Beginning the project tabs.

--- myApp.js
+++ myApp.js
@@ -1,20 +1,22 @@
 var app = angular.module("myApp", ["ngMaterial"]);
 
-app.controller("toDoList", function($scope){
+app.controller("toDoList", function ($scope) {
 	$scope.toDoList = ["Create a company", "Call in barber shop", "Earn a lot of money", "Go to the shop", "Buy gifts", "Brush teeth"];
 	// добавляет элемент в список
-	$scope.addItem = function (){
-		// выдать ошибку если элемент дуплекат
-		$scope.errortext = "";
-		if (!$scope.addMe) {return;}
-		if ($scope.toDoList.indexOf($scope.addMe) == -1){
-			$scope.toDoList.push($scope.addMe);
-		} else {
-			$scope.errortext = "The item is already in your list.";
+	$scope.addItem = function () {
+			// выдать ошибку если элемент дуплекат
+			$scope.errortext = "";
+			if (!$scope.addMe) {
+				return;
+			}
+			if ($scope.toDoList.indexOf($scope.addMe) == -1) {
+				$scope.toDoList.push($scope.addMe);
+			} else {
+				$scope.errortext = "Already in your list.";
+			}
 		}
-	}
-	// удаляет элемент из списка
-	$scope.removeItem = function(item){
+		// удаляет элемент из списка
+	$scope.removeItem = function (item) {
 		$scope.errortext = "";
 		$scope.toDoList.splice(item, 1);
 	}
@@ -22,84 +24,85 @@ app.controller("toDoList", function($scope){
 
 app.controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
 	$scope.toggleLeft = buildDelayedToggler('left');
-    $scope.toggleRight = buildToggler('right');
-    $scope.isOpenRight = function(){
-      return $mdSidenav('right').isOpen();
-    };
-    // User photo and name
-    var imagePath = 'img/userPhoto.jpg';
-    $scope.messages = [{
-      face: imagePath,
-      name: 'Trevor Reyes'   
+	$scope.toggleRight = buildToggler('right');
+	$scope.isOpenRight = function () {
+		return $mdSidenav('right').isOpen();
+	};
+	// User photo and name
+	var imagePath = 'img/userPhoto.jpg';
+	$scope.messages = [{
+		face: imagePath,
+		name: 'Trevor Reyes'
     }];
-    /**
-     * Supplies a function that will continue to operate until the
-     * time is up.
-     */
-    function debounce(func, wait, context) {
-      var timer;
+	/**
+	 * Supplies a function that will continue to operate until the
+	 * time is up.
+	 */
+	function debounce(func, wait, context) {
+		var timer;
 
-      return function debounced() {
-        var context = $scope,
-            args = Array.prototype.slice.call(arguments);
-        $timeout.cancel(timer);
-        timer = $timeout(function() {
-          timer = undefined;
-          func.apply(context, args);
-        }, wait || 10);
-      };
-    }
-    /**
-     * Build handler to open/close a SideNav; when animation finishes
-     * report completion in console
-     */
-    function buildDelayedToggler(navID) {
-      return debounce(function() {
-        // Component lookup should always be available since we are not using `ng-if`
-        $mdSidenav(navID)
-          .toggle()
-          .then(function () {
-            $log.debug("toggle " + navID + " is done");
-          });
-      }, 200);
-    }
-    function buildToggler(navID) {
-      return function() {
-        // Component lookup should always be available since we are not using `ng-if`
-        $mdSidenav(navID)
-          .toggle()
-          .then(function () {
-            $log.debug("toggle " + navID + " is done");
-          });
-      };
-    }
+		return function debounced() {
+			var context = $scope,
+				args = Array.prototype.slice.call(arguments);
+			$timeout.cancel(timer);
+			timer = $timeout(function () {
+				timer = undefined;
+				func.apply(context, args);
+			}, wait || 10);
+		};
+	}
+	/**
+	 * Build handler to open/close a SideNav; when animation finishes
+	 * report completion in console
+	 */
+	function buildDelayedToggler(navID) {
+		return debounce(function () {
+			// Component lookup should always be available since we are not using `ng-if`
+			$mdSidenav(navID)
+				.toggle()
+				.then(function () {
+					$log.debug("toggle " + navID + " is done");
+				});
+		}, 200);
+	}
+
+	function buildToggler(navID) {
+		return function () {
+			// Component lookup should always be available since we are not using `ng-if`
+			$mdSidenav(navID)
+				.toggle()
+				.then(function () {
+					$log.debug("toggle " + navID + " is done");
+				});
+		};
+	}
 });
 
 app.controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.close = function () {
-      // Component lookup should always be available since we are not using `ng-if`
-      $mdSidenav('left').close()
-        .then(function () {
-          $log.debug("close LEFT is done");
-        });
+	$scope.close = function () {
+		// Component lookup should always be available since we are not using `ng-if`
+		$mdSidenav('left').close()
+			.then(function () {
+				$log.debug("close LEFT is done");
+			});
 
-    };
+	};
 });
 app.controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.close = function () {
-      // Component lookup should always be available since we are not using `ng-if`
-      $mdSidenav('right').close()
-        .then(function () {
-          $log.debug("close RIGHT is done");
-        });
-    };
+	$scope.close = function () {
+		// Component lookup should always be available since we are not using `ng-if`
+		$mdSidenav('right').close()
+			.then(function () {
+				$log.debug("close RIGHT is done");
+			});
+	};
 });
 // Configuring theme if needed
-app.config(function($mdThemingProvider) {
-    // Configure a dark theme
-    $mdThemingProvider.theme('myTheme', 'default')
-      .primaryPalette('grey')
-      .dark();
+app.config(function ($mdThemingProvider) {
+	// Configure a dark theme
+	$mdThemingProvider.theme('myTheme', 'default')
+		.primaryPalette('grey')
+		.dark();
 });
 
 /*
@@ -117,5 +120,3 @@ app.directive('userCard', function () {
     }
 });
 */
-
-
