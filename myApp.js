1606ee169f2d7963bca4fc43c2a3a9c9ae1de84d
Islam Dudaev <islamwolf23@gmail.com>
2016-11-10 13:02:30 +0300

    SearchBar and dropdown button complete

--- myApp.js
+++ myApp.js
@@ -25,9 +25,11 @@ app.controller("toDoList", function ($scope) {
 app.controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
 	$scope.toggleLeft = buildDelayedToggler('left');
 	$scope.toggleRight = buildToggler('right');
+	//$scope.toggleRight = buildToggler('newTask');
 	$scope.isOpenRight = function () {
 		return $mdSidenav('right').isOpen();
 	};
+	
 	// User photo and name
 	var imagePath = 'img/userPhoto.jpg';
 	$scope.messages = [{
@@ -97,6 +99,17 @@ app.controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
 			});
 	};
 });
+/*
+app.controller('NewTaskCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+	$scope.close = function () {
+		// Component lookup should always be available since we are not using `ng-if`
+		$mdSidenav('newTask').close()
+			.then(function () {
+				$log.debug("close New Task Sidebar is done");
+			});
+	};
+});
+*/
 // Configuring theme if needed
 app.config(function ($mdThemingProvider) {
 	// Configure a dark theme
@@ -105,18 +118,22 @@ app.config(function ($mdThemingProvider) {
 		.dark();
 });
 
-/*
-app.directive('userCard', function () {
-    return {
-      restrict: 'E',
-      templateUrl: 'userCard.tmpl.html',
-      scope: {
-        name: '@',
-        theme: '@'
-      },
-      controller: function ($scope) {
-        $scope.theme = $scope.theme || 'default';
-      }
-    }
+app.controller('menuController', function menuController ($scope, $mdDialog) {
+	var originatorEv;
+	
+	this.openMenu = function($mdOpenMenu, ev) {
+		originatorEv = ev;
+	    $mdOpenMenu(ev);
+	};
+	
+	this.menuItemClick = function(index) {
+	   $mdDialog.show(
+		  $mdDialog.alert()
+			.title('Hello')
+			.textContent('Menu Item clicked, index: ' + index)
+			.ok('OK')
+			.targetEvent(originatorEv)
+	   );
+	   originatorEv = null;
+	};
 });
-*/
